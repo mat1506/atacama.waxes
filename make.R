f757f0e91e8727ed4a4050c435f2b24a12177c6f
mat1506 <matutefrugone@gmail.com>
2023-01-01 13:37:17 -0300

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' atacama.waxes: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Matías Frugone-Álvarez \email{matutefrugone@gmail.com}
+#' 
+#' @date 2023/01/01
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
